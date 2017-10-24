6b80e98e223d33e75ad8114c0c9691cd7b2b76ce
skobel24 <skobel24@hotmail.ca>
2017-10-24 03:03:42 +0000

    End of Part 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'home', to: 'pages#home'
-  get 'about', to: 'pages#about' 
+  get 'home', to: 'pages#home', as: 'home'
+  get 'about_us', to: 'pages#about', as: 'about'
 
   root to: 'pages#home'
 end
