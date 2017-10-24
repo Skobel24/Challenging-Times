a516d8f3615233eda66177c6d7ea8d1a7f578ed0
skobel24 <skobel24@hotmail.ca>
2017-10-24 03:48:07 +0000

    Finished Part 2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,12 @@
 class PagesController < ApplicationController
-
   def home
-    
   end
 
   def about
+  end
 
+  def lizards
+    @numberOfLizards = params[:number]
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'home', to: 'pages#home', as: 'home'
   get 'about_us', to: 'pages#about', as: 'about'
+  get 'lizards/:number', to: 'pages#lizards', as: 'lizards', id: /\d+/
 
   root to: 'pages#home'
 end
